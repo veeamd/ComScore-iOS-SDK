082a6cb17b5a175023ce39a0a69800690442f955
Illanes, Juan Pablo <jpillanes@comscore.com>
2016-03-22 14:23:49 +0100

    Added umbrella header

---
+++ comScore/ComScore.h
@@ -0,0 +1,27 @@
+//
+//  ComScore-tvOS.h
+//  ComScore-tvOS
+//
+//  Created by Illanes, Juan Pablo on 2/19/16.
+//  Copyright © 2016 comScore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ComScore-tvOS.
+FOUNDATION_EXPORT double ComScore_tvOSVersionNumber;
+
+//! Project version string for ComScore-tvOS.
+FOUNDATION_EXPORT const unsigned char ComScore_tvOSVersionString[];
+
+#import "CSComScore.h"
+#import "CSStreamSense.h"
+#import "CSStreamSenseClip.h"
+#import "CSStreamSenseDefines.h"
+#import "CSStreamSenseDelegate.h"
+#import "CSStreamSensePlaylist.h"
+#import "CSStreamSensePlugin.h"
+#import "CSStreamSensePluginProtocol.h"
+#import "CSStreamSenseState.h"
+#import "CSSessionState.h"
+#import "CSTransmissionMode.h"
